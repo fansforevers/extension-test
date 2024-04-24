bc10097f19e281dfc43d4bf4327a9c020f3b8db4
fansforevers <fansforevers@gmail.com>
2024-04-24 19:12:46 +0700

    first commit

---
+++ src/components/Main/index.jsx
@@ -0,0 +1,7 @@
+export default function Main() {
+  return (
+    <main className="w-full flex justify-center items-center bg-dark p-4">
+      <h1>Wello Hord</h1>
+    </main>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    colors: {
+      white: "#fafafa",
+      dark: "#262626",
+      deep: "#404040",
+      pressure: "#737373",
+      primary: "#1877f2",
+      success: "#16a34a",
+      warning: "#f97316",
+      danger: "#dc2626",
+    },
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
